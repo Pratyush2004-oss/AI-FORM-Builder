0eb55204f168b631b54a5816c8b4b20941c73a14
Pratyush2004-oss <pratyush@sitp.ac.in>
2024-07-13 06:59:07 +0530

    Finalizing the Application

---
+++ app/dashboard/analytics/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Analytics = () => {
+    return (
+        <div className='pb-28 md:pb-0'>
+            <div className='p-10'>
+                <h2 className='text-2xl font-bold'>Analytics </h2>
+                <p className='text-xl font-mono text-error'>Page is not in working for now</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Analytics
